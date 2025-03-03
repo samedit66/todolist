0ba1105e53a8add850d1136e1b6c20fe8d43739f
gorox66 <siotka@list.ru>
2025-03-03 18:46:57 +0300

    Функции done и undone, не доделанные, 1 версия.

--- main.py
+++ main.py
@@ -24,8 +24,20 @@ while exit != True:
     print("Что вы хотите сделать?")
     answer = input("> ")
 
-    
     if answer == "exit":
         print("Вы закончили работу программы")
         exit = True
-        
+
+    def done(number_of_note):
+
+        file = open("tasks.txt", "r", encoding="utf8")
+        list_of_tasks = file.readlines()
+
+        for i in range(0, len(tasks.txt)):
+
+            if i[0] == number_of_note:
+                list_of_found_note = 
+
+            else:
+                print("Номер строчки, которой вы ввели не существует")
+    
